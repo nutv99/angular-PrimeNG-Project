330db41a9a4c9ca891eb021a97e61676105570d7
nutv99 <nutv99@gmail.com>
2022-10-27 12:31:09 +0700

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,32 +6,26 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
-import { AutoCompleteModule } from 'primeng/autocomplete';
-import { CalendarModule } from 'primeng/calendar';
-import { ChipsModule } from 'primeng/chips';
-import { DropdownModule } from 'primeng/dropdown';
-import { InputMaskModule } from 'primeng/inputmask';
-import { InputNumberModule } from 'primeng/inputnumber';
-import { CascadeSelectModule } from 'primeng/cascadeselect';
-import { MultiSelectModule } from 'primeng/multiselect';
-import { InputTextareaModule } from 'primeng/inputtextarea';
+// import { AutoCompleteModule } from 'primeng/autocomplete';
+// import { CalendarModule } from 'primeng/calendar';
+// import { ChipsModule } from 'primeng/chips';
+// import { DropdownModule } from 'primeng/dropdown';
+// import { InputMaskModule } from 'primeng/inputmask';
+// import { InputNumberModule } from 'primeng/inputnumber';
+// import { CascadeSelectModule } from 'primeng/cascadeselect';
+// import { MultiSelectModule } from 'primeng/multiselect';
+// import { InputTextareaModule } from 'primeng/inputtextarea';
 // import { InputTextModule } from "primeng/inputtext";
+
+import { primeModule } from './_shared/prime.module'
 
 @NgModule({
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    AutoCompleteModule,
+    BrowserAnimationsModule,    
     FormsModule,
     HttpClientModule,
-    CalendarModule,
-    ChipsModule,
-    DropdownModule,
-    InputMaskModule,
-    InputNumberModule,
-    CascadeSelectModule,
-    MultiSelectModule,
-    InputTextareaModule,
+    primeModule
   ],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
